e454b5b752f5fffb3d2012cb361170a458b42ab8
TavionK <tavion.k.britt99@gmail.com>
2025-06-29 11:24:42 -0400

    Enhance GPA calculation logic with detailed grade points and new GPA computation functions

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,51 @@ function App() {
   function qualityPonintCalculator(grade, credits) {
     if (grade === "A") {
       return 4 * credits
-    } else if (grade === "B") {
+    } else if (grade === "A-") {
+      return 3.7 * credits
+    } else if (grade === "B+") {
+      return 3.3 * credits
+    }else if (grade === "B") {
       return 3 * credits
+    }else if (grade === "B-") {
+      return 2.7 * credits
+    }else if (grade === "C+") {
+      return 2.3 * credits
     } else if (grade === "C") {
       return 2 * credits
+    }else if (grade === "C-") {
+      return 1.7 * credits
+    }else if (grade === "D+") {
+      return 1.3 * credits
     } else if (grade === "D") {
       return 1 * credits
     } else {
       return 0
     }
+  }
+
+  function getTotalCredits() {
+    let totalCredits = 0
+    grades.forEach((grade) => {
+      totalCredits += grade.credits
+    })
+    return totalCredits
+  }
+
+  function getTotalGradePoints() {
+    let totalGradePoints = 0
+    grades.forEach((grade) => {
+      totalGradePoints += qualityPonintCalculator(grade.grade, grade.credits)
+    })
+    return totalGradePoints
+  }
+
+  function calculateGpa() {
+    let totalCredits = getTotalCredits()
+    let totalGradePoints = getTotalGradePoints()
+
+    return (totalGradePoints / totalCredits).toFixed(2)
+
   }
 
 
@@ -38,6 +74,7 @@ function App() {
       {grades.map((grade) => (
         <p>{qualityPonintCalculator(grade.grade,grade.credits)}</p>
       ))}
+      {calculateGpa()}
     </>
   )
 }
