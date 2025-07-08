f3567dbde3819cb3d60be91eaa4b44871e7d712a
TavionK <tavion.k.britt99@gmail.com>
2025-07-07 23:59:06 -0400

    Show placeholder message when no courses are added

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -3,8 +3,9 @@ import { FormContext } from "../App.jsx";
 
 export default function Courses() {
   const {grades} = React.useContext(FormContext)
+
   return (
-    <>
+     grades.length > 0 ? <>
       {grades.map((grade, index) => (
         <div className={"flex justify-center items-center gap-5"} key={index}>
           <p>{index}</p>
@@ -13,6 +14,6 @@ export default function Courses() {
           <p>{grade.credits}</p>
         </div>
       ))}
-    </>
+    </> : <p>No Courses Added</p>
   )
 }
