80053f63e648143bdd6a3a3aa1be31b5d64df680
TavionK <tavion.k.britt99@gmail.com>
2025-07-09 11:54:12 -0400

    Refactor `Courses` component to implement `removeCourse` functionality and improve key handling by using course index.

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -4,20 +4,18 @@ import { CalculatorContext } from "./Calculator.jsx";
 export default function Courses() {
   const {courses, setCourses} = React.useContext(CalculatorContext)
 
-  // function removeCourse(index){
-  //   setCourses((currentCourses) =>{
-  //     currentCourses.filter((course) => course. !== key)
-  //   })
-  // }
+  function removeCourse(id){
+    setCourses(prevCourses => prevCourses.filter(course => course.id !== id))
+  }
   return (
      courses.length > 0 ? <>
       {courses.map((course, index) => (
         <div className={"flex justify-center items-center gap-5"} key={course.id}>
-          <p>{course.id}</p>
+          <p>{index}</p>
           <p>{course.course}</p>
           <p>{course.grade}</p>
           <p>{course.credits}</p>
-          <button>Remove</button>
+          <button onClick={() => removeCourse(course.id)}>Remove</button>
         </div>
       ))}
     </> : <p>No Courses Added</p>
