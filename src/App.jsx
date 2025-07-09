76249d4c55dd0622c441152221630dbe929a2739
TavionK <tavion.k.britt99@gmail.com>
2025-07-09 13:22:10 -0400

    Refactor GPA calculation logic into dedicated `Gpa` component; update `Calculator` to accept children and simplify structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState, createContext } from 'react'
 import './App.css'
 import Calculator from './components/Calculator.jsx'
-
+import Courses from './components/Courses.jsx'
+import Form from './components/Form.jsx'
+import Gpa from './components/Gpa.jsx'
 function App() {
 
   return (
     <>
       <h1>GPA Calculator</h1>
-      <Calculator />
+      <Calculator>
+        <Form />
+        <Courses />
+        <Gpa />
+      </Calculator>
     </>
   )
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,67 +5,14 @@ import Courses from './Courses.jsx'
 const CalculatorContext = createContext()
 export { CalculatorContext }
 
-export default function Calculator() {
-  const [courses, setCourses] = useState([{id: 0, course: 'Math', grade: "A", credits: 4}])
-
-  function qualityPointCalculator(grade, credits) {
-    if (grade === "A") {
-      return 4 * credits
-    } else if (grade === "A-") {
-      return 3.7 * credits
-    } else if (grade === "B+") {
-      return 3.3 * credits
-    }else if (grade === "B") {
-      return 3 * credits
-    }else if (grade === "B-") {
-      return 2.7 * credits
-    }else if (grade === "C+") {
-      return 2.3 * credits
-    } else if (grade === "C") {
-      return 2 * credits
-    }else if (grade === "C-") {
-      return 1.7 * credits
-    }else if (grade === "D+") {
-      return 1.3 * credits
-    } else if (grade === "D") {
-      return 1 * credits
-    } else {
-      return 0
-    }
-  }
-
-  function getTotalCredits() {
-    let totalCredits = 0
-    courses.forEach((grade) => {
-      totalCredits += grade.credits
-    })
-    return totalCredits
-  }
-
-  function getTotalGradePoints() {
-    let totalGradePoints = 0
-    courses.forEach((grade) => {
-      totalGradePoints += qualityPointCalculator(grade.grade, grade.credits)
-    })
-    return totalGradePoints
-  }
-
-  function calculateGpa() {
-    let totalCredits = getTotalCredits()
-    let totalGradePoints = getTotalGradePoints()
-
-    return (totalGradePoints / totalCredits).toFixed(2)
-
-  }
-
+export default function Calculator({children}) {
+  const [courses, setCourses] = useState([])
 
   return (
     <>
       <CalculatorContext.Provider value={{ courses, setCourses }}>
-        <Form />
-        <Courses />
+          {children}
       </CalculatorContext.Provider>
-      {courses.length > 0 ? calculateGpa() : null}
     </>
   )
 }
