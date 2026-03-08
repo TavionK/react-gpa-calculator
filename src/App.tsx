0147b311ef6ae0ee13f7d445b883dfd25746dcda
TavionK <tavion.k.britt99@gmail.com>
2026-03-08 09:45:01 -0400

    Refactor `App.tsx`: update imports, switch to TypeScript for `Form`, and add `useState` to manage `courses`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import "./App.css";
-import Calculator from "./components/Calculator.js";
-import Courses from "./components/Courses.js";
-import Form from "./components/Form.js";
-import Gpa from "./components/Gpa.js";
+import { useState } from "react";
+import { Course } from "./utils/courses";
+import Form from "./components/Form.tsx";
+
 
 function App() {
+  const [courses, setCourses] = useState<Course[]>([]);
+
   return (
    <main>
      <h1>Hello World</h1>
+     <Form setCourses={setCourses}/>
    </main>
   );
 }
