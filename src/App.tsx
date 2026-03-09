6eab60e39f45fb1f1b0a415b2071248a72874a03
TavionK <tavion.k.britt99@gmail.com>
2026-03-09 13:07:06 -0400

    Add Playfair serif font and update header styles for improved typography

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <main className="max-w-3xl mx-auto px-4 mt-10">
       <div className="flex flex-col items-start gap-4 sm:flex-row sm:justify-between">
         <div>
-          <h1 className="text-3xl font-bold font-serif italic mb-2">
+          <h1 className="text-5xl leading-10 font-bold font-serif italic mb-2">
             Grade Point <br />{" "}
             <span className="text-charged-yellow">Average</span>
           </h1>
