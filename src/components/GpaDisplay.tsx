3fdc727263a4e4b089b0d8f05f290c32793e34f8
TavionK <tavion.k.britt99@gmail.com>
2026-03-08 23:46:04 -0400

    Increase GPA font size in `GpaDisplay` for better visual hierarchy.

--- src/components/GpaDisplay.tsx
+++ src/components/GpaDisplay.tsx
@@ -9,7 +9,7 @@ export default function GpaDisplay({ courses }: GpaDisplayProps) {
   return (
     <div className="bg-gray-900 rounded-md p-6 text-right border border-gray-700">
       <p className="small-text">Current GPA</p>
-      <p className="text-3xl font-bold text-charged-yellow">
+      <p className="text-4xl font-bold text-charged-yellow">
         {calculateGpa(courses) === "NaN" ? "—" : calculateGpa(courses)}
       </p>
       {/*<p className="small-text">*/}
