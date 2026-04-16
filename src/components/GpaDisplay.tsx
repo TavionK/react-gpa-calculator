d4f3b9bbefafa52d8b50fef0398d0343766ee579
TavionK <tavion.k.britt99@gmail.com>
2026-04-16 10:49:35 -0400

    Refine `GpaDisplay` layout with centered alignment and updated separation styling for improved readability

--- src/components/GpaDisplay.tsx
+++ src/components/GpaDisplay.tsx
@@ -7,18 +7,20 @@ interface GpaDisplayProps {
 
 export default function GpaDisplay({ courses }: GpaDisplayProps) {
   return (
-    <div className="bg-blue-500 rounded-md p-6 w-full text-white flex justify-around text-right">
-      <div className="border-r border-white pr-10">
-        <p className="small-text">Current GPA</p>
-        <p className="text-4xl font-bold">
-          {calculateGpa(courses) === "NaN" ? "0.00" : calculateGpa(courses)}
-        </p>
-      </div>
-      <div>
-        <p className="small-text">Total Credits</p>
-        <p className="text-4xl font-bold">
-          {getTotalCredits(courses) === 0 ? "0" : getTotalCredits(courses)}
-        </p>
+    <div className="bg-blue-500 rounded-md p-6 w-full text-white flex justify-center">
+      <div className="flex text-right divide-solid divide-gray-200 divide-x">
+        <div className="px-6">
+          <p className="small-text">Current GPA</p>
+          <p className="text-4xl font-bold">
+            {calculateGpa(courses) === "NaN" ? "0.00" : calculateGpa(courses)}
+          </p>
+        </div>
+        <div className="px-6">
+          <p className="small-text">Total Credits</p>
+          <p className="text-4xl font-bold">
+            {getTotalCredits(courses) === 0 ? "0" : getTotalCredits(courses)}
+          </p>
+        </div>
       </div>
     </div>
   );
