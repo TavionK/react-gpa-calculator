a8c16821fee3082c568fd1f44ff68bbd20c50427
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 19:42:00 -0400

    Add `GpaTrend` component to display GPA trend comparison based on semesters and courses

---
+++ src/components/GpaTrend.tsx
@@ -0,0 +1,33 @@
+import {
+  calculateOverallGpa,
+  calculateCompleteGpa,
+  type Course,
+  type Semester,
+} from "../utils/courses.ts";
+
+interface GpaTrendProps {
+  semesters: Semester[];
+  courses: Course[];
+}
+export default function GpaTrend({ semesters, courses }: GpaTrendProps) {
+  function compareGpa(overallGpa: string, newGpa: string): string {
+    // console.log(overallGpa, newGpa);
+    return (Number(newGpa) - Number(overallGpa)).toFixed(2);
+  }
+
+  return (
+    <section className="w-full mt-15 bg-gray-200 rounded-md overflow-hidden border border-gray-300 p-4">
+      <h2 className="small-text uppercase">GPA Trend</h2>
+      {semesters.length === 0 || courses.length === 0 ? (
+        <p>-</p>
+      ) : (
+        <div className="flex justify-between text-2xl font-bold">
+          {compareGpa(
+            calculateOverallGpa(semesters),
+            calculateCompleteGpa(courses, semesters),
+          )}
+        </div>
+      )}
+    </section>
+  );
+}
