3d7ce876c4f8df33f45dddec30c73f8acac125f4
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 13:43:48 -0400

    Refine `GpaDisplay` layout with improved alignment, updated text labels, and adjusted styling for better readability and clarity

--- src/components/GpaDisplay.tsx
+++ src/components/GpaDisplay.tsx
@@ -9,15 +9,15 @@ export default function GpaDisplay({ courses }: GpaDisplayProps) {
   return (
     <div className="bg-blue-500 rounded-md p-6 w-full text-white flex justify-center">
       <div className="flex text-right divide-solid divide-gray-200 divide-x">
-        <div className="px-6">
-          <p className="small-text">Current GPA</p>
+        <div className="px-6 text-left basis-3/4">
+          <p className="small-text">Current Semester GPA</p>
           <p className="text-4xl font-bold">
             {calculateGpa(courses) === "NaN" ? "0.00" : calculateGpa(courses)}
           </p>
         </div>
-        <div className="px-6">
+        <div className="px-6 basis-1/4">
           <p className="small-text">Total Credits</p>
-          <p className="text-4xl font-bold">
+          <p className="font-bold">
             {getTotalCredits(courses) === 0 ? "0" : getTotalCredits(courses)}
           </p>
         </div>
