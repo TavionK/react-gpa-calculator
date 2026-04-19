cd5ef50a1671d7a01d34d37e9e20d343b347bba0
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 21:02:28 -0400

    Update `OverallGpaDisplay` layout with flex utilities for improved alignment

--- src/components/OverallGpaDisplay.tsx
+++ src/components/OverallGpaDisplay.tsx
@@ -6,7 +6,7 @@ interface FullGpaDisplayProps {
 
 export default function OverallGpaDisplay({ semesters }: FullGpaDisplayProps) {
   return (
-    <section className="w-full mt-15 bg-gray-200 rounded-md overflow-hidden border border-gray-300 p-4">
+    <section className="w-full mt-15 bg-gray-200 rounded-md overflow-hidden border border-gray-300 p-4 flex flex-col justify-end">
       <h2 className="small-text uppercase">Overall GPA</h2>
       {semesters.length === 0 ? (
         <p className="text-2xl font-bold">0.00</p>
