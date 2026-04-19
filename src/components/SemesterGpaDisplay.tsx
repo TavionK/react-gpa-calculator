914135a572f286f7ad4fa5693e109f8998284d91
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 21:29:17 -0400

    Refine `SemesterGpaDisplay` layout and styling for improved readability and alignment

--- src/components/SemesterGpaDisplay.tsx
+++ src/components/SemesterGpaDisplay.tsx
@@ -9,17 +9,17 @@ export default function SemesterGpaDisplay({ courses }: GpaDisplayProps) {
   return (
     <div className="bg-blue-500 rounded-md p-6 w-full text-white flex justify-center">
       <div className="flex text-right divide-solid divide-gray-200 divide-x">
-        <div className="px-6 text-left basis-3/4">
-          <p className="small-text">Current Semester GPA</p>
+        <div className="px-6 text-left basis-2/3">
+          <p className="small-text sm:text-nowrap">Current Semester GPA</p>
           <p className="text-4xl font-bold">
             {calculateSemesterGpa(courses) === "NaN"
               ? "0.00"
               : calculateSemesterGpa(courses)}
           </p>
         </div>
-        <div className="px-6 basis-1/4">
-          <p className="small-text">Total Credits</p>
-          <p className="font-bold">
+        <div className="px-6 basis-1/3">
+          <p className="small-text sm:text-nowrap">Total Credits</p>
+          <p className="text-4xl font-bold">
             {getTotalCredits(courses) === 0 ? "0" : getTotalCredits(courses)}
           </p>
         </div>
