ec2f1557ecd94d4340a02fc41de9a3b9308a6f3d
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 20:42:03 -0400

    Update `OverallGpaDisplay` styling for consistent GPA formatting

--- src/components/OverallGpaDisplay.tsx
+++ src/components/OverallGpaDisplay.tsx
@@ -9,11 +9,9 @@ export default function OverallGpaDisplay({ semesters }: FullGpaDisplayProps) {
     <section className="w-full mt-15 bg-gray-200 rounded-md overflow-hidden border border-gray-300 p-4">
       <h2 className="small-text uppercase">Overall GPA</h2>
       {semesters.length === 0 ? (
-        <p>0.00</p>
+        <p className="text-2xl font-bold">0.00</p>
       ) : (
-        <div className="flex justify-between text-2xl font-bold">
-          {calculatePrevGpa(semesters)}
-        </div>
+        <p className="text-2xl font-bold">{calculatePrevGpa(semesters)}</p>
       )}
     </section>
   );
