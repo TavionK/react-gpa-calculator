95980f0c5d366aaf284212de4dbfc86b5b937c76
TavionK <tavion.k.britt99@gmail.com>
2026-04-19 15:54:36 -0400

    Update empty GPA display to use dash instead of zero for clarity

--- src/components/OverallGpaDisplay.tsx
+++ src/components/OverallGpaDisplay.tsx
@@ -9,7 +9,7 @@ export default function OverallGpaDisplay({ semesters }: FullGpaDisplayProps) {
     <section className="w-full bg-gray-200 rounded-md overflow-hidden border border-gray-300 p-4 flex flex-col justify-end">
       <h2 className="small-text uppercase">Overall GPA</h2>
       {semesters.length === 0 ? (
-        <p className="text-2xl font-bold">0.00</p>
+        <p className="text-2xl font-bold">—</p>
       ) : (
         <p className="text-2xl font-bold">{calculatePrevGpa(semesters)}</p>
       )}
