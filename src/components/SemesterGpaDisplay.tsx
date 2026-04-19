3e00eec335d25a8e6bb8f715aad5d8a3b89c1775
TavionK <tavion.k.britt99@gmail.com>
2026-04-18 20:09:56 -0400

    Rename `GpaDisplay` to `SemesterGpaDisplay` and update to use `calculateSemesterGpa` for alignment with semester-based GPA logic

--- src/components/SemesterGpaDisplay.tsx
+++ src/components/SemesterGpaDisplay.tsx
@@ -1,18 +1,20 @@
 import type { Course } from "../utils/courses";
-import { calculateGpa, getTotalCredits } from "../utils/courses";
+import { calculateSemesterGpa, getTotalCredits } from "../utils/courses";
 
 interface GpaDisplayProps {
   courses: Course[];
 }
 
-export default function GpaDisplay({ courses }: GpaDisplayProps) {
+export default function SemesterGpaDisplay({ courses }: GpaDisplayProps) {
   return (
     <div className="bg-blue-500 rounded-md p-6 w-full text-white flex justify-center">
       <div className="flex text-right divide-solid divide-gray-200 divide-x">
         <div className="px-6 text-left basis-3/4">
           <p className="small-text">Current Semester GPA</p>
           <p className="text-4xl font-bold">
-            {calculateGpa(courses) === "NaN" ? "0.00" : calculateGpa(courses)}
+            {calculateSemesterGpa(courses) === "NaN"
+              ? "0.00"
+              : calculateSemesterGpa(courses)}
           </p>
         </div>
         <div className="px-6 basis-1/4">
