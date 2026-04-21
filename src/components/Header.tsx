3c26d91eaca2d6ab4c167a8ff9853dd5449e418d
TavionK <tavion.k.britt99@gmail.com>
2026-04-20 23:20:20 -0400

    Improve `Header` accessibility: add `aria-label` to `h1`, `aria-hidden` to decorative elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 export default function Header() {
   return (
     <div className="sm:basis-3/4">
-      <h1 className="text-5xl leading-10 font-bold font-serif italic mb-2">
-        Grade Point <br /> <span className="text-blue-500">Average</span>
+      <h1
+        aria-label="Grade point average"
+        className="text-5xl leading-10 font-bold font-serif italic mb-2"
+      >
+        Grade Point <br aria-hidden="true" />
+        <span aria-hidden="true" className="text-blue-500">
+          Average
+        </span>
       </h1>
       <p className="small-text">4.0 Scale - Credit Weighted</p>
     </div>
